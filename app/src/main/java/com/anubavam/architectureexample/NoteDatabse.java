dfc7862a83ef8d812407d93f100ae174513702c5
Venky <venky@anubavam.com>
2019-03-14 18:24:52 +0530

    first commit

---
+++ app/src/main/java/com/anubavam/architectureexample/NoteDatabse.java
@@ -0,0 +1,55 @@
+package com.anubavam.architectureexample;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {Note.class}, version = 1,exportSchema = false)
+public abstract class NoteDatabse extends RoomDatabase {
+
+    private static NoteDatabse instance;
+
+    public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabse getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabse.class,"note_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void>{
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabse noteDatabse){
+            noteDao = noteDatabse.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title1","Description1",1));
+            noteDao.insert(new Note("Title2","Description2",2));
+            noteDao.insert(new Note("Title3","Description3",3));
+            return null;
+        }
+    }
+}
